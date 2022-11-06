7fa0648188da110e3734e0cd6c2b963f4e9386b2
Hyun Jong Kim <hyunjongkim96@gmail.com>
2022-11-05 21:33:37 -0500

    Negligible changes

--- trouver/_modidx.py
+++ trouver/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://hyunjongkimmath.github.io',
                 'git_url': 'https://github.com/hyunjongkimmath/trouver',
                 'lib_path': 'trouver'},
-  'syms': {'trouver.core': {'trouver.core.foo': ('core.html#foo', 'trouver/core.py')}}}
+  'syms': { 'trouver.core': {'trouver.core.foo': ('core.html#foo', 'trouver/core.py')},
+            'trouver.helper': {'trouver.helper.find_regex_in_text': ('helper.html#find_regex_in_text', 'trouver/helper.py')}}}
