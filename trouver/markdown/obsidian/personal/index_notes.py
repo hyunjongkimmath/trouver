91f0a7b5e300bb872098520a95d4c0763eeeb09f
hyunjongkimmath <hyunjongkim96@gmail.com>
2022-12-28 16:59:48 -0800

    "Fix test error in `12_markdown.obsidian.personal.index_notes.ipynb` by making folder nonempty".

--- trouver/markdown/obsidian/personal/index_notes.py
+++ trouver/markdown/obsidian/personal/index_notes.py
@@ -71,7 +71,7 @@ def subsection_folders(
     elif output_type == 'name':
         return [Path(dir).name for dir in glob_result]
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 11
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 12
 def get_alphanumeric(
         title: str, # The title of either a folder or a heading. Must start with an alphanumeric.
         title_type: str # Either `folder` or `heading`.
@@ -92,7 +92,7 @@ def get_alphanumeric(
     
 
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 13
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 14
 def correspond_headings_with_folder(
         index_note: VaultNote,
         vault: PathLike,
@@ -129,7 +129,7 @@ def correspond_headings_with_folder(
     return correspond_dict
     
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 17
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 19
 def information_notes_linked_in_index_note(
         index_note: VaultNote, # The note indexing the information notes.
         vault: PathLike,
@@ -166,7 +166,7 @@ def information_notes_linked_in_index_note(
     return notes_by_headings
     
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 19
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 21
 def move_information_notes_to_correct_folder(
         index_note: VaultNote,
         vault: PathLike,
@@ -196,7 +196,7 @@ def _move_notes_under_heading(
         note.move_to_folder(Path(parent_folder) / destination_folder)
 
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 20
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 22
 def move_information_notes_to_correct_folder_for_all_indices(
         index_of_index_notes: VaultNote, # The index note indexing other index notes; `index_of_index_notes` is intended to be an index note for an entire reference whereas the index notes are intended to correspond to chapters/sections in the reference.
         vault: PathLike,
@@ -217,7 +217,7 @@ def move_information_notes_to_correct_folder_for_all_indices(
         move_information_notes_to_correct_folder(
             index_note, vault, hints=hints+[index_note.rel_path])
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 30
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 32
 def convert_title_to_folder_name(title: str) -> str:
     # TODO: remove left/right
     """
@@ -243,7 +243,7 @@ def convert_title_to_folder_name(title: str) -> str:
         title = title.replace(character, '_')
     return title
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 32
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 34
 def convert_heading_to_folder_name(
         heading: str # Matches regex `\# (\w+?)\. (.*?)`
         ) -> str:
@@ -266,7 +266,7 @@ def convert_heading_to_folder_name(
         #print(heading)
     return f'{alphanumeric}_{convert_title_to_folder_name(title)}'    
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 34
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 36
 def make_folders_from_index_note_headers(
         index_note: VaultNote
         ) -> None:
@@ -288,7 +288,7 @@ def make_folders_from_index_note_headers(
         except OSError as error:
             pass
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 37
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 39
 # TODO: do an example of the `include_embedded_notes` paramtere.
 def get_notes_from_index_note(
         vault: PathLike, # The path to the Obsidian vault directory
@@ -316,7 +316,7 @@ def get_notes_from_index_note(
                        for index_note in index_notes]
     return index_notes
 
-# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 43
+# %% ../../../../nbs/12_markdown.obsidian.personal.index_notes.ipynb 45
 def add_link_in_index_note_after_note_link(
         index_note: VaultNote,
         note_to_add_link_after: VaultNote,
