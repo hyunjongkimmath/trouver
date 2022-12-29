0222155ed6195adb01db7d936dcf81d848b26763
Hyun Jong Kim <hyunjongkim96@gmail.com>
2022-12-28 17:52:36 -0800

    Uncomment the `trouver.markdown.obsidian.personal.notes` import statement in `10_markdown.obsidian.personal.reference.ipynb`

--- trouver/markdown/obsidian/personal/reference.py
+++ trouver/markdown/obsidian/personal/reference.py
@@ -29,9 +29,9 @@ from .authors import find_author_file
 from trouver.markdown.obsidian.personal.index_notes import ( 
     convert_title_to_folder_name
 )
-# from trouver.markdown.obsidian.personal.notes import (
-#     notes_linked_in_note
-# )
+from trouver.markdown.obsidian.personal.notes import (
+    notes_linked_in_note
+)
 # from trouver.markdown.obsidian.personal.note_type import (
 #     type_of_note, PersonalNoteTypeEnum
 # )
